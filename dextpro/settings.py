8e0b3397cc144729078926c310fc8cae10f36d70
maingijustoo <161574493+maingijustoo@users.noreply.github.com>
2025-02-10 17:10:58 +0300

    Update settings.py

--- dextpro/settings.py
+++ dextpro/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY","").strip()
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG= True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+#DEBUG= True
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split() 
 
 
